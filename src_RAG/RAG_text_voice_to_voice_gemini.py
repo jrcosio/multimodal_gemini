c8fdcc3277d9ce88878e581c1c9ba491aea14239
JR Blanco <jr.cosio1@gmail.com>
2025-03-04 22:59:29 +0100

    text_to_voice_RAG

--- src_RAG/RAG_text_voice_to_voice_gemini.py
+++ src_RAG/RAG_text_voice_to_voice_gemini.py
@@ -2,13 +2,13 @@ import asyncio
 import os
 import sys
 import traceback
+
 import pyaudio
 import argparse
+
 from google import genai
 from dotenv import load_dotenv
 from rag import Rag
-
-# https://github.com/GoogleCloudPlatform/generative-ai/blob/main/gemini/multimodal-live-api/real_time_rag_retail_gemini_2_0.ipynb
 
 if sys.version_info < (3, 11, 0):
     import taskgroup, exceptiongroup
@@ -29,6 +29,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=GEMINI_API_KEY, http_options={'api_version': 'v1alpha'})
 
+TIPO = "AUDIO"          #"TEXTO" o "AUDIO"
 
 SYSTEM_MESSAGE = "Eres un asistente experto en tecnología y ayudas a los usuarios siempre en Español."
 
@@ -40,18 +41,45 @@ CONFIG = {
     },
     "generation_config": {
         "response_modalities": ["AUDIO"],
-        "speech_config": "Charon"   #Aoede, Charon, Fenrir, Kore y Puck.
+        "speech_config": "Aoede"   #Aoede, Charon, Fenrir, Kore y Puck.
     }
 }
 
 pya = pyaudio.PyAudio()
 
 class AudioLoop:
-    def __init__(self, rag):
-        self.rag = rag
+    def __init__(self):
         self.audio_in_queue = None
         self.out_queue = None
         self.session = None
+        self.send_text_task = None
+        
+    async def send_text(self):
+        while True:
+            text = await asyncio.to_thread(
+                input,
+                "Mensaje > ",
+            )
+            if text.lower() == "q":
+                break
+            
+             # Obtener fragmentos relevantes
+            context_chunk = rag.get_chunk_relevates(text)
+            context = "\n".join(context_chunk)
+            
+            # Construir el prompt que se enviará a la API
+            mensaje_con_contexto = f"""Eres un asistente de IA que responde basándote en el contexto proporcionado. Si no encuentras información relevante, responde honestamente.
+
+                Contexto:
+                {context}
+
+                Pregunta: {text}
+
+                Respuesta:
+            """
+            
+            
+            await self.session.send(input=mensaje_con_contexto or ".", end_of_turn=True)
         
     
     async def listen_audio(self):
@@ -72,12 +100,9 @@ class AudioLoop:
             await self.out_queue.put({"data": data, "mime_type": "audio/pcm"})
     
     async def send_realtime(self):
-        """
-            Envía mensajes en tiempo real a la API.
-        """
         while True:
             msg = await self.out_queue.get()
-            await self.session.send(input=msg) #<---- Aquí se envía el mensaje a la API.
+            await self.session.send(input=msg)
     
     async def receive_audio(self):
         while True:
@@ -104,10 +129,6 @@ class AudioLoop:
             bytestream = await self.audio_in_queue.get()
             await asyncio.to_thread(stream.write, bytestream)    
         
-        Developed a fully functional Retrieval Augmented Generation (RAG) pipeline capable of answering questions based on provided documents.
-        Demonstrated the versatility of the RAG architecture by enabling both text and audio output modalities.
-        Ensured the adaptability of the RAG pipeline to various use cases by enabling seamless integration of different context documents.
-        Established a foundation for building more advanced RAG systems leveraging larger document sets and sophisticated indexing/retri
     async def run(self):
         try:
             async with (
@@ -118,13 +139,16 @@ class AudioLoop:
                 self.audio_in_queue = asyncio.Queue()
                 self.out_queue = asyncio.Queue(maxsize=5)
 
+                send_text_task = tg.create_task(self.send_text())
                 tg.create_task(self.send_realtime())
                 tg.create_task(self.listen_audio())
                 tg.create_task(self.receive_audio())
                 tg.create_task(self.play_audio())
 
                 # Se mantiene la ejecución hasta que se cancele la tarea.
-                await asyncio.Event().wait()
+                #await asyncio.Event().wait()
+                await send_text_task
+                raise asyncio.CancelledError("User requested exit")
                 
             
 
@@ -137,6 +161,7 @@ class AudioLoop:
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    # Se han eliminado los argumentos de modo (visión)
     args = parser.parse_args()
     
     rag = Rag('./libros/harrypotter_caliz.txt')
